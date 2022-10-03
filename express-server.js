218cb1ddf09aa9fdb12a777fa3f7dfb7a2bd2fbc
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-10-03 16:33:11 +0000

    Implemented starting express server and base directory handling

---
+++ express-server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send(`Hello User:${Math.floor(Math.random() * 10000)}!`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});

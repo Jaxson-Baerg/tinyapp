7235edce130fd189b04e55833be5224407f80178
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-10-03 19:50:27 +0000

    Implemented ejs tutorial files in views folder and functionality in server file

--- express-server.js
+++ express-server.js
@@ -1,17 +1,34 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set('view engine', 'ejs')
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send(`Hello User:${Math.floor(Math.random() * 10000)}!`);
+app.get('/', (req, res) => {
+  const mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  const tagline = "No programming concept is complete without a cute animal mascot!";
+
+  res.render('pages/index', {
+    mascots,
+    tagline
+  });
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 

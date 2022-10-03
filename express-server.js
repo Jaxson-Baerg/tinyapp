15e452feafdf86a98a812846bc59858bdee1eef2
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-10-03 17:03:34 +0000

    added urls.json routing path

--- express-server.js
+++ express-server.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.send(`Hello User:${Math.floor(Math.random() * 10000)}!`);
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
